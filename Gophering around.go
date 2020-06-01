a28dff99500090bee78a88e4ff5e41d02ea019ad
Potts <ianpottinger@gmail.com>
2020-06-01 02:48:13 +0100

    Decision making

--- Gophering around.go
+++ Gophering around.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"strconv"
 	"strings"
 )
@@ -18,7 +19,7 @@ var number int = 42
 var backtick rune = '`'
 var decimal float32 = (float32(number / 10))
 var fourtyTwo string = (strconv.Itoa(number))
-var camelCase = "camel case is used for variables"
+var camelCase string = "camel case is used for variables"
 var state bool = true
 var debug bool = false
 
@@ -37,10 +38,25 @@ var sbyt8x8 int64 = -9223372036854775807
 // var pointer uintptr = *ubyt84
 
 var ieee75432 float32 = 0.0
-var ieee75464 float64 = 0.0
+var ieee75464 float64 = math.Pi
 
-var dfloat32 complex64 = 32.32
+var dfloat32 complex64 = -32.32
 var dfloat64 complex128 = 64.64
+
+var not bool = !false
+var or bool = true || false
+var and bool = true && true
+
+/*
+struct element {
+	key   int
+	value string
+}
+var fixedArray [5]element
+*/
+
+var element int = 0
+var array1D [3]int = [3]int{1, 5, 9}
 
 // comments
 type CaseType struct {
@@ -59,10 +75,10 @@ var slice = []string{"Ready", "Steady", "Go!"}
 func main() {
 	logical := debug == state
 	fmt.Printf("Is %v equal to %v? %v %T\n", state, debug, logical, logical)
-	fmt.Printf("%v, %T\n", purpose, purpose)
+	fmt.Printf("Value %v, of type %T\n", purpose, purpose)
 	fmt.Println(strings.ToUpper(welcome))
 	fmt.Println(fourtyTwo)
-	becomesEqualTo := "Declaration becomes equal to value"
+	becomesEqualTo := "Declaration becomes equal to value\t"
 	fmt.Printf("%v only inside block scopes.\n", becomesEqualTo)
 
 	pascalCase := CaseType{
@@ -71,5 +87,72 @@ func main() {
 	acronyms := CaseType{
 		text: "acronyms(i.e. A.K.A.) should always be capitalised",
 	}
-	fmt.Printf("%v, while %v and %v.", pascalCase.text, camelCase, acronyms.text)
+	fmt.Printf("%v, while %v and %v.\n", pascalCase.text, camelCase, acronyms.text)
+	var concat = fmt.Sprintf("Quoted: %q, while %q and %q.\n", pascalCase.text, camelCase, acronyms.text)
+	fmt.Printf(concat)
+
+	var base2 = fmt.Sprintf("Binary %b ", 2)
+	var base8 = fmt.Sprintf("Octadecimal %o", 8)
+	var base10 = fmt.Sprintf("Decimal %d ", 10)
+	var base16 = fmt.Sprintf("Hexadecimal %X ", 16)
+	fmt.Printf("%v\t %v\t %v\t %v\t", base2, base8, base10, base16)
+
+	fmt.Printf("\nFloat: %e, %e", ieee75432, ieee75464)
+	fmt.Printf("\nSingle: %f, %f", ieee75432, ieee75464)
+	fmt.Printf("\nDouble: %g, %g", ieee75432, ieee75464)
+	fmt.Printf("\nLeft padded %-09.03f", dfloat32)
+	fmt.Printf("\nRight padded %018.06f", dfloat64)
+
+	fmt.Printf("\nNOT ! false: %v", not)
+	fmt.Printf("\nOR || true: %v", or)
+	fmt.Printf("\nAND && true: %v", and)
+
+	if and != or {
+		fmt.Println("\nand is not equal to or")
+	} else {
+		fmt.Println("\nand is equal to or\n")
+	}
+
+	for loop := 0; loop <= 9; loop += 2 {
+		fmt.Printf("Progress %v\n", loop)
+	}
+
+	start := 0
+	limit := 60
+	for {
+		if !(start%3 == 0 || start%5 == 0) {
+			fmt.Printf("%v ", start)
+		}
+		if start%3 == 0 {
+			fmt.Printf("Fizz")
+		}
+		if start%5 == 0 {
+			fmt.Printf("Buzz")
+		}
+		if !(start >= limit) {
+			fmt.Printf("\n")
+			start++
+			continue
+		}
+		if start == limit {
+			fmt.Printf("\nBuzzly all Fizzed out\n")
+			break
+		}
+
+	}
+
+	answer := 5
+	switch answer {
+	case 1:
+		fmt.Println("One")
+	case 10:
+		fmt.Println("Ten")
+	default:
+		fmt.Println("Big")
+
+	}
+
+	fmt.Println(numbers)
+	fmt.Println(array1D)
+
 }
